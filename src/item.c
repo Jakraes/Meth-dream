8b72c2ca6302c853559166aaf8f841baad2a40c9
Jakraes <70046168+Jakraes@users.noreply.github.com>
2024-05-31 04:03:26 +0100

    Yuh

--- src/item.c
+++ src/item.c
@@ -1,12 +1,35 @@
 #include "item.h"
 
 void _create_item_iron_sword(Point position, Item* item) {
-    
+    item->object = gameobject_new(position, &item_iron_sword);
 }
 
 void _create_item(Point position, Item* item) {
+    switch (item->type) {
+        case ITEM_IRON_SWORD:
+            _create_item_iron_sword(position, item);
+            break;
 
+        default:
+            break;
+    }
 }
 
 Item* item_new(Point position, ItemType type) {
+    Item* result = malloc(sizeof(Item));
+
+    result->type = type;
+
+    _create_item(position, result);
+
+    return result;
+}
+
+void item_free(void* obj) {
+    Item* ptr = (Item*) obj;
+
+    if (ptr) {
+        gameobject_free(ptr->object);
+        free(ptr);
+    }
 }
